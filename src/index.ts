ed863ac8510549385b8f26268ba67e64c65d2636
Mason Hipp <mason.hipp@gmail.com>
2023-05-05 13:18:02 -0400

    combining with resizing and url fetching

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,7 @@ export interface Env {
   CACHE_DURATION?: number;
 }
 
-const defaultImageHost = 'imagedelivery.net';
-const maxAge = 30 * 24 * 60 * 60; // 30 day default cache time
+const maxAge = 90 * 24 * 60 * 60; // 180 day default cache time
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
@@ -19,48 +18,92 @@ export default {
 };
 
 async function handleRequest(request: Request, env: Env, ctx: ExecutionContext) {
-  const requestUrl = new URL(request.url);
-  requestUrl.host = env.IMAGE_HOST ?? defaultImageHost;
-  requestUrl.protocol = 'https';
-  requestUrl.port = '443';
+  const url = new URL(request.url);
 
-  const cacheKeyRegex = new RegExp(`\\/${env.IMAGE_ROOT}\\/([\\w\\-]+)\\/([\\w\\-=,]+)\\/?([\\w\\.]+)?`)
-  const [match, imageId, params, name] = cacheKeyRegex.exec(requestUrl.pathname) ?? [];
-  if (!match)
-    return new Response(null, { status: 404, statusText: 'Not Found' });
+  // Cloudflare-specific options are in the cf object.
+  let resizeOptions:any = {}
 
-  const cacheKey = `${env.IMAGE_ROOT}/${imageId}/${params}`;
-  requestUrl.pathname = `/${cacheKey}`;
+  // Copy parameters from query string to request options.
+  // You can implement various different parameters here.
+  if (url.searchParams.has("fit")) resizeOptions.fit = url.searchParams.get("fit") || undefined
+  if (url.searchParams.has("w")) resizeOptions.width = url.searchParams.get("w") || undefined
+  if (url.searchParams.has("h")) resizeOptions.height = url.searchParams.get("h") || undefined
+  if (url.searchParams.has("q")) resizeOptions.quality = url.searchParams.get("q") || undefined
+
+  if(!resizeOptions.fit) {
+    if (url.searchParams.get("enlarge") == 'true') resizeOptions.fit = 'cover'
+  }
+
+  // Your Worker is responsible for automatic format negotiation. Check the Accept header.
+  const accept = request.headers.get("Accept") || ''
+  if (/image\/avif/.test(accept)) {
+    resizeOptions.format = 'avif'
+  } else if (/image\/webp/.test(accept)) {
+    resizeOptions.format = 'webp'
+  }
+
+  // Get URL of the original (full size) image to resize.
+  const imageURLBase64 = url.searchParams.get("src")
+  if (!imageURLBase64) return new Response('Missing "image" value', { status: 400 })
+  const imageURL = atob(imageURLBase64)
+
+  // Check that this is a valid URL
+  try {
+    const { hostname, pathname } = new URL(imageURL)
+
+     if (!/\.(jpe?g|png|gif|webp)$/i.test(pathname)) {
+      return new Response('Disallowed file extension', { status: 400 })
+    }
+
+    if (hostname !== 's3.medialoot.com') {
+     return new Response('Invalid url for source images', { status: 403 })
+    }
+  } catch (err) {
+    return new Response('Invalid "image" value', { status: 400 })
+  }
+
+  
+
+  const optionsKey = Object.entries(resizeOptions).map(([key, val]) => `${key}:${val}`).join('-')
+  const cacheKey = imageURL + optionsKey
   const cache = caches.default;
 
   // Check whether the value is already available in the cache
-  // if not, you will need to fetch it from R2, and store it in the cache
-  let response = await cache.match(requestUrl.toString());
-  if (response) return response;
-  console.log(`Cache miss for: ${cacheKey}.`);
+  // if not, fetch it from R2, and store it in the cache
+  let response = await cache.match(url.toString())
+  if (response) return response
+  console.log(`Cache miss for: ${cacheKey}.`)
 
   // Try to get it from R2
-  let image = await (await env.CACHE_BUCKET.get(cacheKey))?.arrayBuffer();
+  let image = await (await env.CACHE_BUCKET.get(cacheKey))?.arrayBuffer()
   if (!image) {
-    console.log(`R2 miss for: ${cacheKey}.`);
+    console.log(`R2 miss for: ${cacheKey}.`)
 
-    // Get it from CF Images
+    // Get it from CF Images and resize per above options
     let cfImageRes: Response;
     try {
-      cfImageRes = await fetch(requestUrl.toString());
+      // Build a request that passes through request headers
+      const imageRequest = new Request(imageURL, {
+        headers: request.headers
+      })
+
+      cfImageRes = await fetch(imageRequest, {
+        cf: { image: resizeOptions }
+      })
     } catch (e) {
-      return new Response(null, { status: 404, statusText: 'Not Found' });
+      return new Response(null, { status: 404, statusText: 'Not Found' })
     }
-    console.log(`Images fetch response: ${cfImageRes.status}`);
-    image = await cfImageRes.arrayBuffer();
+    console.log(`Images fetch response: ${cfImageRes.status}`)
+    image = await cfImageRes.arrayBuffer()
 
     // If we got it, add it to R2 after we finish
     if (image) {
-      ctx.waitUntil(env.CACHE_BUCKET.put(cacheKey, image));
+      ctx.waitUntil(env.CACHE_BUCKET.put(cacheKey, image))
     }
   }
 
-  if (!image) throw new Error('wut...');
+  // unable to fetch image from R2 or CF or URL, probably bad url
+  if (!image) throw new Error('Unable to fetch image')
 
   // Prep the final response
   const headers = new Headers({
@@ -69,11 +112,12 @@ async function handleRequest(request: Request, env: Env, ctx: ExecutionContext) 
     'access-control-allow-origin': '*',
     'content-security-policy': "default-src 'none'; navigate-to 'none'; form-action 'none'",
     'content-type': 'image/jpeg'
-  });
-  response = new Response(image, { headers });
+  })
+  
+  response = new Response(image)
 
   // Save the response to the cache for next time
-  ctx.waitUntil(cache.put(requestUrl.toString(), response.clone()));
+  ctx.waitUntil(cache.put(url.toString(), response.clone()))
 
-  return response;
+  return response
 }
